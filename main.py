19040132ef9396a402491cabaf40a4f6ebfa61fd
Patricia Pineda <pepineda28@gmail.com>
2023-07-26 23:18:24 +0800

    pseudocode

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 # Name: Pineda, Patricia Anne E.
 # Section: BSCPE 1-5
 # OOP Final Project: Covid Contact Tracing App with GUI
+
+# import tkinter
+# create window and frame
+# user info
+# first name
+# last name
+# gender
+# age
+# occupation
+# contact details
+# address
+# number
+# email
+# health info
+# sore throat
+# fever
+# cough
+# runny nose
+# loss of smell
+# loss of taste
+# abdominal pain
+# diarrhea
+# accept terms
